f86e4ce4d2ed1458a9ea557ebb79bc035e3f16cb
IT-Edris <adrismalya@gmail.com>
2022-12-21 09:14:50 +0430

    Html parser package added

--- resources/js/Components/Text.jsx
+++ resources/js/Components/Text.jsx
@@ -1,24 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import { usePage } from '@inertiajs/inertia-react'
+import parse from 'html-react-parser'
 
-const Text = ({ text, variables }) => {
-    const { translations } = usePage().props
-    const [textString, setTextString] = useState(text)
-
-    /*useEffect(() => {
-        let outputString = text.replace(/ /g, '-').toLowerCase()
-        if (typeof translations[outputString] === 'undefined') {
-            setText(children)
-        } else {
-            let string = translations[outputString]
-            for (let key in variables) {
-                string = string.replace(`[${key}]`, variables[key])
-            }
-            setText(string)
-        }
-    }, [])
-    return <>{text}</>*/
-    return textString
+const Text = ({ text }) => {
+    return <div className={'prose'}>{parse(text)}</div>
 }
 
 export default Text
